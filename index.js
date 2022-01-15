ccd4eb47ae51d6871c28a9b7e846234da060e9d8
Hadeel <hadeel@safraapp.com>
2022-01-15 19:19:04 +0300

    added

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ function fillButton(index, text) {
 }
 // pre-made a function. You can use this function to present an alert to say someone wins
 function winningAlert(winner) {
-  confirm(`Horraaay, ${winner} wins!`);
+  if(confirm(`Horraaay, ${winner} wins!`)){
+    restartGame();
 }
 
 // SAMPLE CODE: This code fills the 1st and 9th button with X and O initially
@@ -23,36 +24,43 @@ function winningAlert(winner) {
  * That interacts with the UI
  */
 
-let playerOne = "X";
-let playerTwo = "O";
-let turn = 0;
-let lastClicked;
-let onePos = [];
-let twoPos = [];
-let index1 = [];
-let index2 = [];
-let winningPositions = [[1,4,7],[2,5,8],[3,6,9],[1,2,3],[4,5,6],[7,8,9],[1,5,9],[3,5,7]];
 
+let counter = 0;
+let grid = ["","","","","","","","",""];
 function clickButton(index) {
-  console.log(`Button number ${index} is clicked`);
   // Your main code here.
   // player = checkPlayer();
   // fillButton(index, player);
-  if (turn % 2 === 0) {
-    fillButton(index, playerOne);
-    lastClicked = playerOne;
-    index1.push(index);
-    console.log(index1);
-    checkWinner();
-  } else {
-    fillButton(index, playerTwo);
-    lastClicked = playerTwo;
-    index2.push(index)
-    console.log(index2);
-    checkWinner();
-  }
-  turn += 1;
+  if (grid[index - 1] === "") {
+   
+  const currentPlayer = counter % 2 == 0 ? "X" : "O";}
+  grid[index - 1] = currentPlayer;
+  fillButton(index, currentPlayer);
+  checkWinner (currentPlayer);
+// end of turn
+  counter++;
+  console.log(`Button number ${index} is clicked
+  counter: ${counter}
+  grid: ${grid}`);
 }
+
+
+
+  // another way
+  // if (counter % 2 === 0) {
+  //   fillButton(index, playerOne);
+  //   lastClicked = playerOne;
+    
+  //   // console.log(index1);
+  //   // checkWinner();
+  // } else {
+  //   fillButton(index, playerTwo);
+  //   lastClicked = playerTwo;
+    
+  //   // console.log(index2);
+  //   // checkWinner();
+
+
 
 /**
  * 👇🏻 BELOW are functions that you CAN use to structure your code properly.
@@ -71,16 +79,42 @@ function checkPlayer() {
  * who is winning and returns the winner
  */
 // function checkWinner
-function checkWinner() {
-
-  if (lastClicked=playerOne) {
-    index1.forEach()
+function checkWinner(p) {
+     
+   
     
-  } else {
+    const Row1 = [g[0], g[1], g[2]].join("") === [p,p,p].join("");
+   
+    const Row2 = [grid[3], grid[4] , grid[5]].join("") === [p,p,p].join("");
     
+    const Row3 = [grid[6], grid[7] , grid[8]].join("") === [p,p,p].join("");
+    
+    const C1 = [grid[0], grid[3], grid[6]].join("") === [p,p,p].join("");
+    
+    const C2 = [grid[1], grid[4], grid[7]].join("") === [p,p,p].join("");
+  
+    const C3 = [grid[2], grid[5], grid[8]].join("") === [p,p,p].join("");
+   
+    const Diag1 = [grid[0], grid[4], grid[8]].join("")=== [p,p,p].join("");
+   
+    const Diag2 = [grid[2], grid[4], grid[6]].join("")=== [p,p,p].join("");
+    
+    if (Row1||Row2||Row3||C1||C2||C3||Diag1||Diag2) {
+    winningAlert(p);
   }
-
-
 }
 
-// function restartGame
+
+
+
+
+function restartGame(){
+counter = 0;
+grid = ["","","","","","","","",""];
+for(let i=0; i<9; i++){
+  
+fillButton(i, "");
+}
+}
+}
+
